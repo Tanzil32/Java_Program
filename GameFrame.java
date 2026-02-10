22c6ab4277123a9b9e163f48892f857c802015bc
Tanzil32 <tanzil3246@gmail.com>
2026-02-10 19:01:28 +0600

    Snake Game

---
+++ GameFrame.java
@@ -0,0 +1,20 @@
+package project;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+	
+	GameFrame(){
+		
+		
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack(); // window preferred size 
+		this.setVisible(true);
+		this.setLocationRelativeTo(null); // center on screen
+		
+	}
+
+}
---
+++ SnakeGame.java
@@ -0,0 +1,10 @@
+package project;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		new GameFrame();
+	}
+
+}
