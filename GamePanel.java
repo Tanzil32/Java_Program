2c97a5d969adccc5da325a5f79922bd0f6bb3e39
Tanzil32 <tanzil3246@gmail.com>
2026-02-10 19:09:33 +0600

    Update

--- GamePanel.java
+++ GamePanel.java
@@ -13,7 +13,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int SCREEN_HEIGHT =600;
 	static final int UNIT_SIZE =20;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-	static final int DELAY=90;// snake speed
+	static final int DELAY=92;// snake speed
 	final int x[] = new int[GAME_UNITS];// part of snake position in x and y
 	final int y[] = new int[GAME_UNITS];
 	int bodyParts = 6;
